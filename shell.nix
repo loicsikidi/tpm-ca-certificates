01a92a3c480a75c2d3af152515842fd2d1bb266c
loicsikidi <loic.sikidi@gmail.com>
2025-12-02 07:54:17 +0100

    first commit

---
+++ shell.nix
@@ -0,0 +1,21 @@
+{
+  pkgs ?
+    import (fetchTarball
+      "https://github.com/NixOS/nixpkgs/archive/ee09932cedcef15aaf476f9343d1dea2cb77e261.tar.gz")
+    {},
+}: let
+  helpers = import (
+    builtins.fetchTarball "https://github.com/loicsikidi/nix-shell-toolbox/tarball/main"
+  ) {inherit pkgs;};
+in
+  pkgs.mkShell {
+    buildInputs = with pkgs;
+      [goreleaser cosign syft]
+      ++ helpers.packages;
+
+    shellHook = ''
+      ${helpers.shellHook}
+      echo "Development environment ready!"
+      echo "  - Go version: $(go version)"
+    '';
+  }
