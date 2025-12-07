5dfe4954329445261ac23f16d074c26e9be614ec
loicsikidi <loic.sikidi@gmail.com>
2025-12-07 23:03:12 +0100

    fix: regression

--- internal/github/client.go
+++ internal/github/client.go
@@ -17,7 +17,7 @@ var SourceRepo = Repo{Owner: "loicsikidi", Name: "tpm-ca-certificates"}
 
 const (
 	ReleaseBundleWorkflowPath = ".github/workflows/release-bundle.yaml"
-	githubAPIBaseURL          = "https://apiv1beta.github.com"
+	githubAPIBaseURL          = "https://api.github.com"
 	apiVersion                = "2022-11-28"
 )
 
