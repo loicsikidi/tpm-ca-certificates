6740bacfb85e11fc6907cbe60b5d05b8fc4acfb3
loicsikidi <loic.sikidi@gmail.com>
2025-12-11 08:05:11 +0100

    wip: custom shell hooks

--- shell.nix
+++ shell.nix
@@ -11,12 +11,44 @@
       gotest.settings.flags = "-short -race";
     };
   };
+
+  # Import git-hooks.nix directly for local custom hooks
+  nix-pre-commit-hooks = import (
+    builtins.fetchTarball "https://github.com/cachix/git-hooks.nix/tarball/50b9238891e388c9fdc6a5c49e49c42533a1b5ce"
+  );
+
+  # Local custom hooks specific to this project
+  localHooks = nix-pre-commit-hooks.run {
+    src = ./.;
+    hooks = {
+      tpm-config-format = {
+        enable = true;
+        name = "TPM Config Format";
+        description = "Check TPM config files formatting";
+        files = "^\\.tpm-(roots|intermediates)\\.yaml$";
+        entry = "${pkgs.go}/bin/go run ./ config format --dry-run --config";
+        language = "system";
+        pass_filenames = true;
+      };
+
+      tpm-config-validate = {
+        enable = true;
+        name = "TPM Config Validate";
+        description = "Validate TPM config files";
+        files = "^\\.tpm-(roots|intermediates)\\.yaml$";
+        entry = "${pkgs.go}/bin/go run ./ config validate --quiet --config";
+        language = "system";
+        pass_filenames = true;
+      };
+    };
+  };
 in
   pkgs.mkShell {
     buildInputs = with pkgs; [goreleaser cosign syft gcc] ++ helpers.packages;
 
     shellHook = ''
       ${helpers.shellHook}
+      ${localHooks.shellHook}
       echo "Development environment ready!"
       echo "  - Go version: $(go version)"
     '';
